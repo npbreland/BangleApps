3c72f0af01b3eae25ab13e12d78a701bd515a120
Nick Breland <npbreland@tutanota.com>
2023-06-25 20:55:23 +0100

    Add customiser

--- apps/habitbuilder/app.js
+++
@@ -1,8 +0,0 @@
-// place your const, vars, functions or classes here
-
-// Create the file in append mode
-const file = require("Storage").open("habitbuilder.json", "a");
-
-// Load widgets
-Bangle.loadWidgets();
-Bangle.drawWidgets();

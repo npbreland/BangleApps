3e1a33a00caec4ba62654ed64fe2098258671e5b
Nick Breland <npbreland@tutanota.com>
2023-09-16 17:59:19 +0100

    Use Layout properly

--- apps/habitbuilder/app.js
+++ apps/habitbuilder/app.js
@@ -1,22 +1,27 @@
-const Storage = require("Storage");
-const Layout = require("Layout");
+var Layout = require("Layout");
+var settings = require("Storage").readJSON("habitbuilder.settings.json");
+var dataFile = require("Storage").open("habitbuilder.data.csv", "a");
 
-const filename = "habitbuilder.settings.json";
-const settings = Storage.readJSON(filename);
-
-const dataFile = Storage.open("habitbuilder.data.csv", "a");
-
-function showQuestion(questionIdx) {
-  const layout = new Layout( {
+var layout = new Layout(
+  {
     type: "v", c: [
-      {type:"txt", wrap: true, fillx: 1, height: 50, font:"6x8:2", label:settings.questions[questionIdx], id:questionIdx},
-      {type: "h", c: [
-        {type:"btn", font:"6x8:2", pad:2, width: 75, label:"Yes", cb: l=>setAnswer(questionIdx, 1) },
-        {type:"btn", font:"6x8:2", pad:2, width: 75, label:"No", cb: l=>setAnswer(questionIdx, 0) }
-      ]}
-    ]
+        {type:"txt", wrap: true, fillx: 1, height: 50, font:"6x8:2", label:settings.questions[0], id:"question"},
+      ],
+  },
+  {
+    btns: [
+      {id:"yes", label:"Yes", cb: l=>print('yes')},
+      {id:"no", label:"No", cb: l=>print('no')}
+    ],
+    lazy: true
   });
-  g.clear();
+
+layout.update();
+
+function drawQuestion(questionIdx) {
+  layout.question.label = settings.questions[questionIdx];
+  layout.yes.cb = l => setAnswer(questionIdx, 1);
+  layout.no.cb = l => setAnswer(questionIdx, 0);
   layout.render();
 }
 
@@ -25,54 +30,20 @@ function getDateString(date) {
 }
 
 function setAnswer(questionIdx, answer) {
-  const date = new Date();
-  const dateStr = getDateString(date);
-  const question = settings.questions[questionIdx];
+  var date = new Date();
+  var dateStr = getDateString(date);
+  var question = settings.questions[questionIdx];
 
-  const data = [question, dateStr, answer];
+  var data = [question, dateStr, answer];
   dataFile.write(data.join(",") + "\n");
 
   if (settings.questions[questionIdx + 1]) {
-    showQuestion(questionIdx + 1);
+    drawQuestion(questionIdx + 1);
   } else {
+    Bangle.setUI();
     E.showMessage("All done for today!");
   }
 }
 
-/*
-Might use these for a widget later
-
-function getFeaturedQuestion() {
-  const entries = Object.entries(questionData);
-  let question, data;
-  for (let i = 0; i < entries.length; i++) {
-    question = entries[i][0];
-    data = entries[i][1];
-    if (data.featured === true) {
-      return question;
-    }
-  }
-  return false;
-}
-
-
-function getStreak(question, endDate) {
-  const responses = questionData[question].responses;
-  let streak = 0;
-  const date = endDate;
-  let dateString = getDateString(date);
-
-  while (responses[dateString] && responses[dateString] === true) {
-    streak++;
-    date.setDate(date.getDate() - 1);
-    dateString = getDateString(date);
-  }
-  return streak;
-}
-*/
-
-showQuestion(0);
-
-// Load widgets
-Bangle.loadWidgets();
-Bangle.drawWidgets();
+g.clear();
+drawQuestion(0);
